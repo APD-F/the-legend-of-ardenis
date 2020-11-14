0853920d06c1ed8be48c045a25afbdc65b8aac0b
Sean Mang <seanmang7@gmail.com>
2020-11-14 02:00:16 -0800

    Added Slime idle/walking sprites and animations. Added slime movement controls

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float moveSpeed = 5f;
+    public Rigidbody2D rigBody;
+    public Animator animator;
+    Vector2 movement;
+
+    // Update is called once per frame. This function handles input
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    // executed on fixed timer, which is more reliable when dealing with physics. This function handles movement
+    void FixedUpdate()
+    {
+    	rigBody.MovePosition(rigBody.position + (movement * moveSpeed * Time.fixedDeltaTime));
+    }
+}
